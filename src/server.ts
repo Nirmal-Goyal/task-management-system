05afb86c80c953ddc6e50a6509abf6be54ede05f
Nirmal <nirmalgoyal0108@gmail.com>
2026-02-07 10:57:46 +0530

    creating database config file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import app from './app'
 import dotenv from "dotenv"
+import connectDB from './config/db'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () =>{
-    console.log(`Server is running on server ${PORT}`)
-})
+const startServer = async () => {
+    await connectDB()
+
+    app.listen(PORT, () =>{
+        console.log(`Server is running on server ${PORT}`)
+    })
+}
+
+startServer()
