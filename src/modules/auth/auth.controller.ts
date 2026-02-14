dfc0f65702f270d61851ad2c4fc6de3ea21dc466
Nirmal <nirmalgoyal0108@gmail.com>
2026-02-14 11:30:53 +0530

    feat: Login API + JWT generation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import {Request, Response} from "express"
 import User from './user.models'
 import { registerSchema } from "./auth.schema"
+import jwt from "jsonwebtoken"
+import { loginSchema } from "./auth.schema"
 
 export const registerUser = async (req: Request, res: Response) => {
     const parsed = registerSchema.safeParse(req.body)
@@ -33,5 +35,42 @@ export const registerUser = async (req: Request, res: Response) => {
             name: user.name,
             email: user.email
         }
+    })
+}
+
+export const loginUser = async (req: Request, res: Response) => {
+    const parsed = loginSchema.safeParse(req.body)
+    if(!parsed.success){
+        return res.status(400).json({
+            message: "Validation failed",
+            errors: parsed.error.issues,
+        })
+    }
+
+    const {email, password} = parsed.data
+
+    const user = await User.findOne({email})
+
+    if(!user){
+        return res.status(401).json({message: "Invalid credentials"})
+    }
+
+    const isMatch = await user.comparePassword(password)
+
+    if(!isMatch){
+        return res.status(401).json({
+            message: "Invalid password"
+        })
+    }
+
+    const token = jwt.sign(
+        {userId: user._id},
+        process.env.JWT_SECRET as string,
+        {expiresIn: "1d"}
+    );
+
+    res.json({
+        message: "Login successfull",
+        token,
     })
 }
--- src/modules/auth/auth.schema.ts
+++ src/modules/auth/auth.schema.ts
@@ -1,4 +1,4 @@
-import {z} from "zod"
+import {email, z} from "zod"
 
 export const registerSchema = z.object({
     name: z.string().min(2, "Name is too short"),
@@ -6,4 +6,10 @@ export const registerSchema = z.object({
     password: z.string().min(6, "Password must be atleast 6 characters"),
 })
 
+export const loginSchema = z.object({
+    email: z.string().email("Invalid email"),
+    password: z.string().min(6)
+})
+
 export type RegisterInput = z.infer<typeof registerSchema>
+export type LoginInput = z.infer<typeof loginSchema>
