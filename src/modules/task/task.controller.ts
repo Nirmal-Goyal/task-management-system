b3dfc0c8239840db4ad4118b09eab7d9ff25a1da
Nirmal <nirmalgoyal0108@gmail.com>
2026-02-15 21:10:55 +0530

    feat: creating api for getting single task

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -2,7 +2,8 @@ import { Response } from "express";
 import Task from "./task.models";
 import { createTaskSchema } from "./task.schema";
 import { AuthRequest } from "../../middlewares/auth.middleware";
-import { count } from "node:console";
+import mongoose from "mongoose";
+
 
 export const createTask = async (req: AuthRequest, res: Response) => {
     const parsed = createTaskSchema.safeParse(req.body)
@@ -33,4 +34,28 @@ export const getAllTasks = async (req: AuthRequest, res: Response) => {
         count: tasks.length,
         tasks
     })
+}
+
+export const getSingleTask = async (req: AuthRequest, res: Response) => {
+    const id = req.params.id as string;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({
+            message: "Task not found"
+        })
+    }
+
+    const task = await Task.findById(id)
+
+    if(!task){
+        return res.status(404).json({
+            message: "Task not found"
+        })
+    }
+
+    if(task.userId.toString()!==req.userId){
+        return res.status(403).json({message: "Forbidden"})
+    }
+
+    res.json(task)
 }
--- src/modules/task/task.routes.ts
+++ src/modules/task/task.routes.ts
@@ -2,9 +2,12 @@ import { Router } from "express";
 import { createTask } from "./task.controller";
 import { authMiddleware } from "../../middlewares/auth.middleware";
 import { getAllTasks } from "./task.controller";
+import { getSingleTask } from "./task.controller";
+
 
 const router = Router();
 router.post("/", authMiddleware, createTask)
 router.get("/", authMiddleware, getAllTasks)
+router.get("/:id", authMiddleware, getSingleTask)
 
 export default router;
